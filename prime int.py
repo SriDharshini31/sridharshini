666b407aeca1814fe850d3c411b8f592eba39f40
SriDharshini31 <53745361+SriDharshini31@users.noreply.github.com>
2019-08-12 14:34:16 +0530

    Create prime int.py

---
+++ prime int.py
@@ -0,0 +1,9 @@
+lower = 1  
+upper = 10
+for n in range(lower,upper + 1):  
+   if n > 1:  
+       for i in range(2,n):  
+           if (n % i) == 0:  
+               break  
+       else:
+       	   print(n)  
