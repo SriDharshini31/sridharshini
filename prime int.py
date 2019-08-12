a4dda75765939e55253c27a943a622bd5deb89b7
SriDharshini31 <53745361+SriDharshini31@users.noreply.github.com>
2019-08-12 15:24:56 +0530

    Delete prime int.py

--- prime int.py
+++
@@ -1,9 +0,0 @@
-lower = 1  
-upper = 10
-for n in range(lower,upper + 1):  
-   if n > 1:  
-       for i in range(2,n):  
-           if (n % i) == 0:  
-               break  
-       else:
-       	   print(n)  
