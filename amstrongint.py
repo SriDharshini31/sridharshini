f26193eba8d08445aee20c74ec43838ce5a4b112
SriDharshini31 <53745361+SriDharshini31@users.noreply.github.com>
2019-08-14 13:43:32 +0530

    Create amstrongint.py

---
+++ amstrongint.py
@@ -0,0 +1,11 @@
+lower=int(input())
+upper=int(input())
+for num in range(lower,upper+1):
+  temp=num
+  sum=0
+  while temp>0:
+      digit=temp%10
+      sum=sum+digit**3
+      temp=temp//10
+      if sum==num:
+           print (num)
