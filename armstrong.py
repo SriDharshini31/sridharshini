31adf25bbbd346240eab1a7ad868f458633a52ea
SriDharshini31 <53745361+SriDharshini31@users.noreply.github.com>
2019-08-14 13:40:37 +0530

    Create armstrong.py

---
+++ armstrong.py
@@ -0,0 +1,11 @@
+n= int(input(""))
+length = len(str(n))
+sum = 0
+temp = n
+while(temp != 0):
+	sum = sum + ((temp % 10) ** length)
+	temp = temp // 10
+if sum == n:
+	print("yes")
+else:
+	print("no")
